005e9354bd4479b34f67cae97586ac336707fffc
Jonathan Wells <jonwells90@gmail.com>
2026-07-23 08:05:51 -0400

    Create malloc helpers

--- include/utils.h
+++ include/utils.h
@@ -7,5 +7,8 @@ typedef struct {
     size_t j;
 } index_t;
 
+void *xmalloc(size_t size);
+void *xcalloc(size_t len, size_t size);
+void *xrealloc(void *ptr, size_t size);
 index_t *argmax(size_t m, size_t n, size_t **array_2d);
 int compare_strlen(const void * a, const void * b);
--- src/utils.c
+++ src/utils.c
@@ -4,13 +4,36 @@
 
 #include "utils.h"
 
-/* Return the (i, j) index of the maximum value in array_2d (caller frees). */
-index_t *argmax(size_t m, size_t n, size_t **array_2d) {
-    index_t *index = malloc(sizeof(index_t));
-    if (!index) {
+void *xmalloc(size_t size) {
+    void *buffer = malloc(size);
+    if (!buffer) {
         perror("malloc");
         exit(1);
     }
+    return buffer;
+}
+
+void *xcalloc(size_t len, size_t size) {
+    void *buffer = calloc(len, size);
+    if (!buffer) {
+        perror("calloc");
+        exit(1);
+    }
+    return buffer;
+}
+
+void *xrealloc(void *ptr, size_t size) {
+    void *buffer = realloc(ptr, size);
+    if (!buffer) {
+        perror("realloc");
+        exit(1);
+    }
+    return buffer;
+}
+
+/* Return the (i, j) index of the maximum value in array_2d (caller frees). */
+index_t *argmax(size_t m, size_t n, size_t **array_2d) {
+    index_t *index = (index_t *)xmalloc(sizeof(index_t));
     *index = (index_t){0, 0};
 
     size_t currmax = 0;
@@ -26,6 +49,7 @@ index_t *argmax(size_t m, size_t n, size_t **array_2d) {
     return index;
 }
 
+/* Return the length difference between two strings. */
 int compare_strlen(const void * a, const void * b) {
     const char *sa = *(char **)a;
     const char *sb = *(char **)b;
